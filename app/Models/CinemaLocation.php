1966fbd6c8698f50581d2b5143c7ba5f2279e778
Chris Miracle <55501479+Chris-Miracle@users.noreply.github.com>
2021-12-29 02:30:32 +0100

    declare the eloquent relationship

--- app/Models/CinemaLocation.php
+++ app/Models/CinemaLocation.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class CinemaLocation extends Model
 {
     use HasFactory;
+
+    public function movies(){
+        return $this->hasMany('App\Models\Movie');
+    }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'movie_time',
+        'show_date',
+        'show_time',
+        'image1',
+    ];
+
+    public function cinema_locatiom(){
+        return $this->belongsTo('App\Models\CinemaLocation');
+    }
 }
